3ff3da7ee2abea6c3fb95fdb81ed85c981aad426
OzanUlus <ilkerozanulus@gmail.com>
2024-04-02 12:43:41 +0300

    added dbcontext

---
+++ OU.JwtApp.Back/Persistance/Context/JwtContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OU.JwtApp.Back.Core.Domain;
+using System.Reflection;
+
+namespace OU.JwtApp.Back.Persistance.Context
+{
+    public class JwtContext : DbContext
+    {
+        public JwtContext(DbContextOptions<JwtContext> options) : base(options)
+        {
+
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppRole> AppRoles { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+       
+    }
+}
--- OU.JwtApp.Back/Program.cs
+++ OU.JwtApp.Back/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using OU.JwtApp.Back.Persistance.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<JwtContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
